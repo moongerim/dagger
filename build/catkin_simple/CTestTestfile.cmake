4f73ac31c032d0ba4880885e6f798a2ecabca472
Moongerim <aigerim.nurbayeva@nu.edu.kz>
2021-07-16 17:25:07 +0600

    build devel

---
+++ build/catkin_simple/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/robot/workspaces/dagger/src/catkin_simple
+# Build directory: /home/robot/workspaces/dagger/build/catkin_simple
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ build/mpc_pr/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/robot/workspaces/dagger/src/mpc_pr
+# Build directory: /home/robot/workspaces/dagger/build/mpc_pr
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
