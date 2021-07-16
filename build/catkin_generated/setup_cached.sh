2f04f3738f890f5e5145d8a6f57e54ebc6276e7e
Moongerim <aigerim.nurbayeva@nu.edu.kz>
2021-07-16 17:27:54 +0600

    build devel

--- build/catkin_generated/setup_cached.sh
+++ build/catkin_generated/setup_cached.sh
@@ -8,6 +8,8 @@
 
 # modified environment variables
 export CMAKE_PREFIX_PATH="/home/robot/workspaces/dagger/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/home/robot/workspaces/dagger/devel/lib:$LD_LIBRARY_PATH"
+export PKG_CONFIG_PATH="/home/robot/workspaces/dagger/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
 export PWD='/home/robot/workspaces/dagger/build'
 export ROSLISP_PACKAGE_DIRECTORIES='/home/robot/workspaces/dagger/devel/share/common-lisp'
 export ROS_PACKAGE_PATH="/home/robot/workspaces/dagger/src:$ROS_PACKAGE_PATH"
